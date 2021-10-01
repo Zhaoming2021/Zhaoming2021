f958393cd9150fa170f8529c8f5179ad806b0de3
Zhaoming2021 <91507587+Zhaoming2021@users.noreply.github.com>
2021-10-01 18:12:07 -0500

    final test

---
+++ survey1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  1 13:57:12 2021
+
+@author: zhaomingli
+"""
+
+# python survey.py
+
+name = input("What is your name")
+print(name);
+
+program = input("Tell me some basic infomation about your program.")
+print(program);
+
+interest = input("What are your academic interests? ")
+
+skill = input("What programming languages do you have experience with?")
+
+background = input("What is your experience with Python?")
+
+zone = input("What time zone are you in? ")
+
+hope = input("What is something you would like to learn in this course?")
+
+concern = input("Do you have any questions or concerns you would like to share?")
+
+
---
+++ test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  1 15:40:52 2021
+
+@author: zhaomingli
+"""
+
+# python survey.py
+
+name = str(input('what is your name'))
+print(name)
+
+program = str(input("Tell me some basic infomation about your program."))
+print(program)
+
+interest = str(input("What are your academic interests? "))
+print(interest)
+
+skill = str(input("What programming languages do you have experience with?"))
+
+
+background = str(input("What is your experience with Python?"))
+
+
+zone = str(input("What time zone are you in? "))
+
+
+hope = str(input("What is something you would like to learn in this course?"))
+
+
+concern = str(input("Do you have any questions or concerns you would like to share?"))
+
